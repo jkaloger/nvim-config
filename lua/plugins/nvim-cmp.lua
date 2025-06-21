d87bb340356f05925ff142c40ae69fdee581b0a1
Jack Kaloger <jkaloger@users.noreply.github.com>
2025-06-21 15:33:36 +1000

    fix: clean up lsp config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -24,11 +24,4 @@ return {
       }
     })
   end,
-  opts = function(_, opts)
-    opts.sources = opts.sources or {}
-    table.insert(opts.sources, {
-      name = "lazydev",
-      group_index = 0,   -- set group index to 0 to skip loading LuaLS completions
-    })
-  end
 }
